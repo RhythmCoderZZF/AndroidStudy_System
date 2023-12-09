46b357618555b493cf077471b8650c713721f784
zzf <958665147@qq.com>
2023-12-09 18:08:25 +0800

    usb module

---
+++ app/src/main/java/com/zzf/studysystem/permission/PermissionActivity.java
@@ -0,0 +1,16 @@
+package com.zzf.studysystem.permission;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zzf.studysystem.R;
+
+public class PermissionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permission);
+    }
+}
